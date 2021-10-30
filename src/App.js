2a55e425f7fe8e664509ae6f9411818e5922a23e
Eghinner <eghinner@gmail.com>
2021-10-30 10:22:02 -0700

    🎨Add styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styled from '@emotion/styled';
+
+// Styled
+const Contenedor = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 3rem;
+  flex-direction: column;
+`;
+
+const Boton = styled.button`
+  background: -webkit-linear-gradient(top left,#b69800 0%, #b69800 40%, #735d00 100%) no-repeat;
+  /*background-size: 300px;*/
+  font-family: Arial, Helvetica, sans-serif;
+  color: #fff;
+  margin-top: 3rem;
+  padding: .5rem 1.5rem;
+  font-size: 2rem;
+  border: 2px solid black;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <React.Fragment>
+      <Contenedor>
+        <Boton>
+          Cambiar Frase
+        </Boton>
+      </Contenedor>
+    </React.Fragment>
+  )
 }
 
 export default App;
